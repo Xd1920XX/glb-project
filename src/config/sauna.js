ac38dceadb87fbd00896eb809bbc99b4e7474883
Karl-Sten Suurna <karl-sten.suurna@delfi.ee>
2026-04-21 15:46:12 +0300

    Fix

--- src/config/sauna.js
+++ src/config/sauna.js
@@ -33,7 +33,7 @@ export const MODELS = {
     interiors: [
       { id: 'huum-drop',     label: 'Huum Drop 9kw WI-FI',         icon: encode('/Ikoonid/Huum.jpg'),       path: encode('/City_Elegant/CIty Elegant_6k_Huum Drop.jpg') },
       { id: 'huum-drop-eos', label: 'Huum Drop 9kw WI-FI + Eos',   icon: encode('/Ikoonid/HUUM + EOS.jpg'), path: encode('/City_Elegant/CIty Elegant_6k_Huum Drop+EOS.jpg') },
-      { id: 'huum-flow-eos', label: 'Huum Flow + Eos',             icon: encode('/Ikoonid/HUUM + EOS.jpg'), path: encode('/City_Elegant/CIty Elegant_6k_Huum Flow+EOS.jpg') },
+      { id: 'huum-flow-eos', label: 'Huum Flow Mini 8.5kw + EOS',  icon: encode('/Ikoonid/HUUM HIVE Flow Mini + EOS.jpg'), path: encode('/City_Elegant/CIty Elegant_6k_Huum Flow+EOS.jpg') },
     ],
   },
   'city-xs': {
