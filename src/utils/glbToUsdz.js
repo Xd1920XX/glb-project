2ccf9f185c0c6be3f97ff2b3d8fa0f19ca4d5ef1
Karl-Sten Suurna <karl-sten.suurna@delfi.ee>
2026-05-28 10:36:07 +0300

    FIx

--- src/utils/glbToUsdz.js
+++ src/utils/glbToUsdz.js
@@ -1,8 +1,14 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import { USDZExporter } from 'three/examples/jsm/exporters/USDZExporter.js'
 
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
 const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
 const textureLoader = new THREE.TextureLoader()
 textureLoader.crossOrigin = 'anonymous'
 
